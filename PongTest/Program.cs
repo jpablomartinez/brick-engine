cb494d1152eb9bb6153f503e2daf1e5a3c996759
jpablomartinez <jp.martinez.colombo@gmail.com>
2024-11-05 21:43:25 -0300

    test opengl

--- PongTest/Program.cs
+++ PongTest/Program.cs
@@ -4,7 +4,7 @@ namespace BrickEngine{
 
     class Program{
         static void Main(string[] args){
-            using(var game = new GraphicsManager(800,600,"BrickEngine")){
+            using(var game = new GraphicsManager(800,600,"Brick Engine - Pong Game")){
                 game.Run();
             }
         }
