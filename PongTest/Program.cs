848349ce5651f48eec87c17cd43e1f059c60d86b
jpablomartinez <jp.martinez.colombo@gmail.com>
2024-11-23 10:31:23 -0300

    improve color class

--- PongTest/Program.cs
+++ PongTest/Program.cs
@@ -5,9 +5,9 @@ namespace BrickEngine{
     class Program{
         static void Main(string[] args){
 
-            Color backgroundColor = Color.FromHexColor(0x020919);            
+            //Color backgroundColor = Color.FromHexColor(0x020919);                        
 
-            using(var game = new GraphicsManager(800,600,"Brick Engine - Pong Game", backgroundColor)){
+            using(var game = new GraphicsManager(800,600,"Brick Engine - Pong Game", Color.BrickColor)){
                 game.Run();
             }
         }
